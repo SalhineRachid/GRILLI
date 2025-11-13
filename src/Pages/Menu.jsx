15e52b4795c9425f5f8aed3c42ec8aaa273ef1e2
salhine rachid <salhinerachid@gmail.com>
2025-11-14 00:31:12 +0100

    working on the moving background components

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -6,60 +6,70 @@ import menu3 from "../assets/menu-3.png";
 import menu4 from "../assets/menu-4.png";
 import menu5 from "../assets/menu-5.png";
 import menu6 from "../assets/menu-6.png";
+import shape5 from "../assets/shape-5.png"
+import shape6 from "../assets/shape-6.png"
 function Menu() {
   return (
-    <div className="h-[110vh] w-[100%] flex justify-center items-center bg-[#171616] ">
-      <div className="w-[30%] h-[40%] flex flex-col justify-between items-center text-white">
-        <MenuItem
-          dishPicture={menu1}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
-        <MenuItem
-          dishPicture={menu2}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
-        <MenuItem
-          dishPicture={menu3}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
+    <div className="relative h-[110vh] w-[100%] flex justify-center items-center bg-[#171616]">
+      <img className="absolute inset-0 z-0 strechInOut" src={shape5}/>
+      <div className="absolute w-[800px] h-[450px] bottom-0 right-0 ">
+        <img className="absolute w-[100%] strechInOut  z-50" src={shape6}/>
       </div>
-      <div  className="w-[30%] h-[40%] flex flex-col justify-between items-center text-white">
-        <MenuItem
-          dishPicture={menu4}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
-        <MenuItem
-          dishPicture={menu5}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
-        <MenuItem
-          dishPicture={menu6}
-          dishName={"Greek Salad"}
-          dishPrice={25.0}
-          dishIng={
-            "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
-          }
-        />
+
+      <div className="w-[100%] h-[100%] flex justify-center items-center absolute z-10">
+        <div className="w-[30%] h-[40%] flex flex-col justify-between items-center gap-15 text-white">
+          <MenuItem
+            dishPicture={menu1}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+          <MenuItem
+            dishPicture={menu2}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+          <MenuItem
+            dishPicture={menu3}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+        </div>
+
+        <div  className="w-[30%] h-[40%] flex flex-col justify-between items-center gap-15 text-white">
+          <MenuItem
+            dishPicture={menu4}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+          <MenuItem
+            dishPicture={menu5}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+          <MenuItem
+            dishPicture={menu6}
+            dishName={"Greek Salad"}
+            dishPrice={25.0}
+            dishIng={
+              "Tomatoes, green bell pepper, sliced cucumber onion, olives, and feta cheese."
+            }
+          />
+        </div>
       </div>
     </div>
   );
