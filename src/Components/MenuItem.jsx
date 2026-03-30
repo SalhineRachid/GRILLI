643c2dbed2d785208e857ba041d6809e02d4177d
salhine rachid <salhinerachid@gmail.com>
2026-03-30 22:15:17 +0100

    update package dependencies and adjust MenuItem component styling for improved layout

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function MenuItem({ dishName, dishPrice, dishIng, dishPicture }) {
   return (
-    <div className="w-full max-w-[400px] h-[80px] flex gap-[15px]">
+    <div className="w-full max-w-[450px] h-[80px] flex gap-[15px]">
       <img className="w-[25%] h-[100%]" src={dishPicture} />
-      <div className="flex flex-col justify-center gap-[8px] text-[#A6A6A6]">
-        <div className="flex justify-between gap-[3px]">
+      <div className="flex flex-col justify-center items-start gap-[8px] text-[#A6A6A6] text-left">
+        <div className="flex justify-between w-full gap-[3px]">
           <h4 id="apply-forum-font" className="text-base sm:text-lg md:text-xl text-white hover:text-[hsl(38,61%,73%)] hover:cursor-pointer">{dishName}</h4>
          
           <h3 id="apply-forum-font" className="text-base sm:text-lg md:text-xl text-[hsl(38,61%,73%)]">{dishPrice}</h3>
